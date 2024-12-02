5455ddc09165ba4b6d2e7291b691980c8073d138
Okafor Kenechukwu Howell <62034993+CaesyDev@users.noreply.github.com>
2024-12-02 10:45:35 +0100

    Update bulb.js

--- bulb.js
+++ bulb.js
@@ -1,1 +1,30 @@
-(function(){var _0x5a46=['getElementById','src','style','display','none','innerHTML','Bulb\x20is\x20now\x20on','innerText','block','pic_bulbon.gif','message','Bulb\x20is\x20now\x20off','pic_bulboff.gif'];(window['\x5fO0O']=function(_0x4a80c9){return _0x4a80c9;})();function _0x1c3b(_0x3f5430,_0x5c5251){var _0x4e5c3f=_0x5a46;return _0x1c3b=function(_0x4c7b98,_0x19c3dc){_0x4c7b98=_0x4c7b98-0x0;var _0x2c6cbb=_0x4e5c3f[_0x4c7b98];return _0x2c6cbb;},_0x1c3b(_0x3f5430,_0x5c5251);}function switchon(){var _0x1ed2ff=document[_0x1c3b('0x0')]('bulb');_0x1ed2ff[_0x1c3b('0x1')]=_0x1c3b('0x9');var _0x1ed2f0=document[_0x1c3b('0x0')]('onbutton');_0x1ed2f0[_0x1c3b('0x2')][_0x1c3b('0x3')]=_0x1c3b('0x4');var _0x1ed2f1=document[_0x1c3b('0x0')]('offbutton');_0x1ed2f1[_0x1c3b('0x2')][_0x1c3b('0x3')]=_0x1c3b('0x8');var _0x1ed2f2=document[_0x1c3b('0x0')](_0x1c3b('0xa'));_0x1ed2f2[_0x1c3b('0x5')]=_0x1c3b('0x6');setTimeout(function(){_0x1ed2f2[_0x1c3b('0x7')]='';},0xbb8);}function switchoff(){var _0x1ed2ff=document[_0x1c3b('0x0')]('bulb');_0x1ed2ff[_0x1c3b('0x1')]=_0x1c3b('0xb');var _0x1ed2f0=document[_0x1c3b('0x0')]('onbutton');_0x1ed2f0[_0x1c3b('0x2')][_0x1c3b('0x3')]=_0x1c3b('0x8');var _0x1ed2f1=document[_0x1c3b('0x0')]('offbutton');_0x1ed2f1[_0x1c3b('0x2')][_0x1c3b('0x3')]=_0x1c3b('0x4');var _0x1ed2f2=document[_0x1c3b('0x0')](_0x1c3b('0xa'));_0x1ed2f2[_0x1c3b('0x5')]=_0x1c3b('0xc');setTimeout(function(){_0x1ed2f2[_0x1c3b('0x7')]='';},0xbb8);}window['\x5fO0O']&&window['\x5fO0O']();})();
+function switchon(){
+    let bulb = document.getElementById("bulb");
+    bulb.src = "pic_bulbon.gif";
+    let onbutton = document.getElementById("onbutton");
+    onbutton.style.display = "none";
+    let offbutton = document.getElementById("offbutton");
+    offbutton.style.display = "block";
+
+    let text = document.getElementById("message");
+    text.innerHTML = "Bulb is now on";
+    setTimeout(function() {
+        text.innerText = "";
+    }, 3000)
+}
+
+function switchoff(){
+    let bulb = document.getElementById("bulb");
+    bulb.src = "pic_bulboff.gif";
+   
+    let onbutton = document.getElementById("onbutton");
+    onbutton.style.display = "block";
+    let offbutton = document.getElementById("offbutton");
+    offbutton.style.display = "none";
+
+    let text = document.getElementById("message");
+    text.innerHTML = "Bulb is now off";
+    setTimeout(function() {
+        text.innerText = "";
+    }, 3000)
+}
